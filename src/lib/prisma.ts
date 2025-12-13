1dc9d3b17259f16624f8e57c12bfae693393c394
Arnold Agaba <arnoldagaba2@gmail.com>
2025-12-13 22:43:23 +0300

    initial commit

---
+++ src/lib/prisma.ts
@@ -0,0 +1,15 @@
+import { neonConfig } from "@neondatabase/serverless";
+import { PrismaNeon } from "@prisma/adapter-neon";
+import ws from "ws";
+import { PrismaClient } from "@/generated/prisma/client";
+
+neonConfig.webSocketConstructor = ws;
+neonConfig.poolQueryViaFetch = true;
+
+const adapter = new PrismaNeon({
+    connectionString: process.env.DATABASE_URL,
+});
+
+const prisma = new PrismaClient({ adapter });
+
+export default prisma;
---
+++ src/routes/_protected/route.tsx
@@ -0,0 +1,13 @@
+import { createFileRoute, Outlet } from "@tanstack/react-router";
+import { authMiddleware } from "@/middleware/auth";
+
+export const Route = createFileRoute("/_protected")({
+    component: RouteComponent,
+    server: {
+        middleware: [authMiddleware],
+    },
+});
+
+function RouteComponent() {
+    return <Outlet />
+}
