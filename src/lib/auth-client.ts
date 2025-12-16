277e6b8eb8852bdeeb45c1a5e57cbe17e8030f48
Arnold Agaba <arnoldagaba000@gmail.com>
2025-12-16 09:14:45 +0300

    chore: Infer types from auth

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,8 +1,12 @@
-import { lastLoginMethodClient } from "better-auth/client/plugins";
+import {
+    inferAdditionalFields,
+    lastLoginMethodClient,
+} from "better-auth/client/plugins";
 import { createAuthClient } from "better-auth/react";
+import type { auth } from "./auth";
 
 export const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
     // baseURL: "http://localhost:3000",
-    plugins: [lastLoginMethodClient()],
+    plugins: [inferAdditionalFields<typeof auth>(), lastLoginMethodClient()],
 });
