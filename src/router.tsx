cfb82d0d3b0f09140877e8225af34aec64c1776b
Arnold Agaba <arnoldagaba000@gmail.com>
2025-12-17 18:14:51 +0300

    feat: add react query feature

--- src/router.tsx
+++ src/router.tsx
@@ -1,14 +1,24 @@
 import { createRouter } from "@tanstack/react-router";
+import { setupRouterSsrQueryIntegration } from "@tanstack/react-router-ssr-query";
+import { getContext } from "@/integrations/tanstack-query/root-provider";
 
 // Import the generated route tree
 import { routeTree } from "./routeTree.gen";
 
 // Create a new router instance
 export const getRouter = () => {
+    const rqContext = getContext();
+
     const router = createRouter({
         routeTree,
+        context: { ...rqContext },
         scrollRestoration: true,
         defaultPreloadStaleTime: 0,
+    });
+
+    setupRouterSsrQueryIntegration({
+        router,
+        queryClient: rqContext.queryClient,
     });
 
     return router;
